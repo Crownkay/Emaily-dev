b8b09e6742a1611b8d5fdc1df42b5d105484890a
Crownkay <adekomsk@gmail.com>
2020-07-30 10:56:32 +0100

    passport strategy

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const express = require('express');
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require('./config/keys')
+
 const app = express();
 
- app.get('/', (req, res)=>{
-     res.send({ bye: "buddy" })
- });
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret
+}));
 
- const PORT = process.env.PORT || 5000;
- app.listen(PORT);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
